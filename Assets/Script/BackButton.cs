646985b6a32d456915439dde3e657b2654377a37
linhbh02 <linhbhce160280@fpt.edu.vn>
2023-10-10 06:53:30 +0700

    init project

---
+++ Assets/Script/BackButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class BackButton : MonoBehaviour
+{
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(1); // Load scene menu khi nhấn nút button
+    }
+}
